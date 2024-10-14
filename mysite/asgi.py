98c6b5a941a64d5357c2d7c0c64630610b213eff
yvilaca <lucas.vilaca@forteplus.com.br>
2024-10-14 11:31:43 -0300

    teste

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+""" A """
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_asgi_application()
